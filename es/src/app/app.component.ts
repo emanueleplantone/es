fce331d6e69ae9bf4e43bfa333996be10b3d670b
emanueleplantone <plantone.emanuele@iisgalvanimi.edu.it>
2022-03-08 17:29:57 +0000

    commit 2

--- es/src/app/app.component.ts
+++ es/src/app/app.component.ts
@@ -7,4 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'es';
+  testo:string = "ciao";
+  lista:string[]=[ 'Banane' , 'lamponi' , 'latte' , 'sale'  ]
+
 }
